3d124265f9b0cc37ee129a287a8d838ccd9064b3
Sennof <141116442+Sennof@users.noreply.github.com>
2026-03-30 12:02:45 +0300

    comments pt2 (#5)
    Co-authored-by: dkroshnon <dkroshnin@yandex.ru>

--- Scripts/data_loader.py
+++ Scripts/data_loader.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
+# Uploading data from CSV
 def load_data():
-    """Загружает плановые и фактические данные из CSV."""
     menu_plan = pd.read_csv('menu_plan.csv')
     sales_fact = pd.read_csv('sales_fact.csv')
     return menu_plan, sales_fact
